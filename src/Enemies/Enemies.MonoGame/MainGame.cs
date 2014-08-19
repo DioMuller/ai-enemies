2f90a2c9a6b05b4c2a500851e4ca844a13230114
Joao Vitor P. Moraes <joao.moraes@lactec.org.br>
2014-08-19 10:58:31 -0300

    Created solution with shared projects.

---
+++ src/Enemies/Enemies.MonoGame/MainGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Enemies.MonoGame
+{
+    public class MainGame : Enemies.MainGame
+    {
+        public MainGame()
+        {
+            Graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = 800,
+                PreferredBackBufferHeight = 600,
+                IsFullScreen = false,
+            };
+
+            Content.RootDirectory = "Content";
+
+            // Linux MonoGame Bug ( https://github.com/mono/MonoGame/issues/628 )
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += (sender, e) => {
+                Window.AllowUserResizing = false;
+            };
+        }
+    }
+}
+
