990cbcc6be363e9a84e9149705f9241023ec7302
Joao Vitor P. Moraes <jvlppm@gmail.com>
2014-09-07 04:36:46 -0300

    Added GUI base.
    Current elements supported:
     - Xamarin.Forms.Label
     - Xamarin.Forms.StackLayout
     - Enemies.Controls.ImageButton
    
    Usage sample:
    new Xamarin.Forms.Label { Text = "Test" }.AsEntity()

---
+++ src/Enemies/Enemies.Shared/Controls/ImageButton.cs
@@ -0,0 +1,32 @@
+﻿namespace Enemies.Controls
+{
+    using Microsoft.Xna.Framework.Graphics;
+    using System;
+    using Xamarin.Forms;
+
+    public class ImageButton : Label
+    {
+        public static BindableProperty ImageNormalProperty = BindableProperty.Create<ImageButton, string>(p => p.ImageNormal, defaultValue: null);
+        public string ImageNormal
+        {
+            get { return (string)GetValue(ImageNormalProperty); }
+            set { SetValue(ImageNormalProperty, value); }
+        }
+
+        public static BindableProperty ImageOverProperty = BindableProperty.Create<ImageButton, string>(p => p.ImageOver, defaultValue: null);
+        public string ImageOver
+        {
+            get { return (string)GetValue(ImageOverProperty); }
+            set { SetValue(ImageOverProperty, value); }
+        }
+
+        public event EventHandler OnClick;
+
+        public void FireClicked()
+        {
+            if (OnClick != null)
+                OnClick(this, EventArgs.Empty);
+        }
+    }
+}
+
