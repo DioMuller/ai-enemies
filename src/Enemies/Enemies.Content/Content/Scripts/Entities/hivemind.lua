e4d5db560901508b07344fb043e3e5f35a7ae1fe
Diogo Muller <diogo.muller@lactec.org.br>
2014-09-30 16:13:00 -0300

    WIP: Hivemind entity.

---
+++ src/Enemies/Enemies.Content/Content/Scripts/Entities/hivemind.lua
@@ -0,0 +1,35 @@
+﻿script = {}
+
+script.current_target = nil
+
+function script.Initialize()
+
+end
+
+function script.DoUpdate(delta)
+	if script.current_target then
+		local movement = steering.seek(entity.Position, script.current_target, 1)
+		
+		entity:Move(movement.X, movement.Y)
+	end
+end
+
+function script.DoUpdate(delta)
+
+end
+
+function script.ReceiveMessage(message)
+	if message.Text == "follow" then
+		local newPos = message.Attachment
+
+		if newPos then
+			if not script.current_target then 
+				script.current_target = newPos
+			elseif vector.distance(entity:Position, script.current_target) > vector.distance(entity:Position, newPos) then
+				script.current_target = newPos
+			end
+		end
+	end
+end
+
+return script
--- src/Enemies/Enemies.Content/Content/Scripts/Libs/vector.lua
+++ src/Enemies/Enemies.Content/Content/Scripts/Libs/vector.lua
@@ -49,4 +49,12 @@ function vector.multiply(v, c)
 	return result
 end
 
+function vector.square_distance(v1, v2)
+	return ((v1.X - v2.X) * (v1.X - v2.X)) + ((v1.Y - v2.Y) * (v1.Y - v2.Y))
+end
+
+function vector.distance(v1, v2)
+	return math.sqrt( vector.square_distance(v1, v2) )
+end
+
 return vector
