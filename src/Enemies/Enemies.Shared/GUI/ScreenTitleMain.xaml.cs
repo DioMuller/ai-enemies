c342d1951734febcfc1565543e86e56309ac145c
Diogo Muller <diogo.muller@live.com>
2014-10-06 21:22:27 -0300

    Layout changes.

--- src/Enemies/Enemies.Shared/GUI/ScreenTitleMain.xaml.cs
+++ src/Enemies/Enemies.Shared/GUI/ScreenTitleMain.xaml.cs
@@ -9,6 +9,7 @@ namespace Enemies.GUI
     public partial class ScreenTitleMain
     {
         public OnButtonClickDelegate OnNewGame;
+		public OnButtonClickDelegate OnSandbox;
         public OnButtonClickDelegate OnLoadGame;
         public OnButtonClickDelegate OnQuitGame;
 
@@ -21,6 +22,11 @@ namespace Enemies.GUI
         {
             if (OnNewGame != null) OnNewGame();
         }
+
+		public void Sandbox_Click(object sender, EventArgs e)
+		{
+			if (OnSandbox != null) OnSandbox();
+		}
 
         public void LoadGame_Click(object sender, EventArgs e)
         {
