f07d8e92601b3912627ddee7da88516cfb1f0d6e
Diogo Muller <diogo.muller@lactec.org.br>
2014-12-11 16:25:32 -0200

    WIP: Part of the astar implementation.

--- src/Enemies/Enemies.Content/Content/Scripts/Libs/astar.lua
+++ src/Enemies/Enemies.Content/Content/Scripts/Libs/astar.lua
@@ -58,21 +58,67 @@ function astar.findPath(mapData, position, objective)
 
 	local close = {}
 	local open = {}
+	local running = true
 
 	-- 1 - Add Start Node to the OPEN List.
+	table.insert(open, start)
 	-- 2 - Repeat:
+	while running do
 		-- 2.a - Search Node with the lowest F cost on the OPEN list.
+		local currentNode = nil
+		local indexNode = -1
+		local currentIndex = 0
+
+		for _, node in ipairs ( open ) do
+			if currentNode then
+				if currentNode.f > node.f then
+					currentNode = node
+					indexNode = currentIndex
+				end
+			else
+				currentNode = node
+				indexNode = currentIndex
+			end
+
+			currentIndex = currentIndex + 1
+		end
 		-- 2.b - Move this node to the CLOSE list.
+		table.insert(close, start)
+		table.remove(open, indexNode)
+
 		-- 2.c - For each neighbour node:
-			-- 2.c.0 - Calculate Heuristic
-			-- 2.c.i - Ignore if it's an obstacle or if it's already on the CLOSE list.
-			-- 2.c.ii - If it's not on OPEN, add to open and set the Node PARENT to CURRENT. Record f, g and h.
-			-- 2.c.iii - If it's already on OPEN, check if the new path is better. If it is, change the recorded PARENT, f, g and h values.
+		for i = -1, 2 do
+			iPos = i + currentNode.position.X
+
+			if iPos >= 0 and iPos < grid.width then
+				for j = -1, 2 do
+					jPos = j + currentNode.position.Y
+					if jPos >= 0 and jPos < grid.height then
+						-- 2.c.0 - Calculate Heuristic
+						-- 2.c.i - Ignore if it's an obstacle or if it's already on the CLOSE list.
+						-- 2.c.ii - If it's not on OPEN, add to open and set the Node PARENT to CURRENT. Record f, g and h.
+						-- 2.c.iii - If it's already on OPEN, check if the new path is better. If it is, change the recorded PARENT, f, g and h values.
+				end 
+			end
+		end
+
 		-- 2.d - Stop the process when:
+		if running then
 			-- 2.d.i - Found! Target Node is on CLOSE.
+			if currentNode.type == NodeType.Target then running = false end
 			-- 2.d.ii - Not Found! OPEN is empty.
+			if table.getn(open) == 0 then running = false end
+		end
+
+	end
+
 	-- 3 - Generate a path using the PARENT nodes.
 	result = {}
+
+	while currentNode and not currentNode.type == NodeType.Start do
+		table.insert(result, currentNode)
+		currentNode = currentNode.parent
+	end
 
 	-- 4 - Return the result.
 	return result
