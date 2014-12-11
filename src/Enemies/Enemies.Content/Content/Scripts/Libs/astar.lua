f8fb2b1e3f56f2171914eb7ad9a633e4eabbdc21
Diogo Muller <diogo.muller@lactec.org.br>
2014-12-11 16:56:26 -0200

    WIP: Astar in Lua, untested.

--- src/Enemies/Enemies.Content/Content/Scripts/Libs/astar.lua
+++ src/Enemies/Enemies.Content/Content/Scripts/Libs/astar.lua
@@ -1,6 +1,19 @@
-﻿astar = {}
+﻿-- Aux: Table.Contains
+function table.contains(table, element)
+  for _, value in pairs(table) do
+    if value == element then
+      return true
+    end
+  end
+  return false
+end
 
-NodeType =
+astar = {}
+
+astar.directWeight = 10
+astar.diagonalWeight = 14
+
+astar.NodeType =
 {
 	Start = 'X',
     Path = '-',
@@ -8,19 +21,30 @@ NodeType =
 	End = 'O'
 }
 
-function createNode(position, f, g, h)
+function astar.createNode(position, f, g, h)
 	return
 	{
 		position = position,
-		f = 0,
-		g = 0,
+		f = function()
+			return g() + h
+		end,
+		g = function()
+			if type == astar.NodeType.Start then return 0 end
+			if not parent then return 0 end
+
+			if position.X == parent.Position.X or position.Y == parent.Position.Y then
+				return parent.g() + astar.directWeight
+			else
+				return parent.g() + astar.diagonalWeight
+			end
+		end,
 		h = 0,
 		parent = nil,
-		type = NodeType.Path
+		type = astar.NodeType.Path
 	}
 end
 
-function createGrid(mapData)
+function astar.createGrid(mapData)
 	grid = {}
 
 	grid.width = mapData.TileCount.X
@@ -30,12 +54,12 @@ function createGrid(mapData)
 		grid[i] = {}
 		for j=1, mapData.TileCount.Y do
 			-- Create node base
-			grid[i][j] = createNode(position, f, g, h)
+			grid[i][j] = astar.createNode(position, f, g, h)
 			
 			if mapData.CanGo(i, j) then
-				grid[i][j].type = NodeType.Path
+				grid[i][j].type = astar.NodeType.Path
 			else
-				grid[i][j].type = NodeType.Obstacle
+				grid[i][j].type = astar.NodeType.Obstacle
 			end
 		end
 	end
@@ -43,18 +67,18 @@ function createGrid(mapData)
 	return grid
 end
 
-function astar.findPath(mapData, position, objective)
+function astar.findPath(mapData, position, objective, heuristic)
 	-- Position and Objective Quad Calculation
-	local startPos = createNode(mapData:GetQuadrantOf(position.X, position.Y))
+	local startPos = astar.createNode(mapData:GetQuadrantOf(position.X, position.Y))
 	local targetPos = mapData:GetQuadrantOf(objective.X, objective.Y)
 
 	-- 0 - Initialization
-	nodes = createGrid(mapData)
+	nodes = astar.createGrid(mapData)
 	start = nodes[startPos.X][startPos.Y]
 	target = nodes[targetPos.X][targetPos.Y]
 
-	start.type = NodeType.Start
-	target.type = NodeType.End
+	start.type = astar.NodeType.Start
+	target.type = astar.NodeType.End
 
 	local close = {}
 	local open = {}
@@ -71,7 +95,7 @@ function astar.findPath(mapData, position, objective)
 
 		for _, node in ipairs ( open ) do
 			if currentNode then
-				if currentNode.f > node.f then
+				if currentNode.f() > node.f() then
 					currentNode = node
 					indexNode = currentIndex
 				end
@@ -95,9 +119,32 @@ function astar.findPath(mapData, position, objective)
 					jPos = j + currentNode.position.Y
 					if jPos >= 0 and jPos < grid.height then
 						-- 2.c.0 - Calculate Heuristic
+						nbNode = nodes[iPos][jPos]
+						nbNode.h = heuristic(nbNode, start, target)
+						
+						local pass = false
+						 
 						-- 2.c.i - Ignore if it's an obstacle or if it's already on the CLOSE list.
+						if nbNode.type == astar.NodeType.Obstacle or table.contains(close, nbNode) then
+							pass = true
+						end
 						-- 2.c.ii - If it's not on OPEN, add to open and set the Node PARENT to CURRENT. Record f, g and h.
+						if not pass and not table.contains(open, nbNode ) then
+							nbNode.parent = currentNode
+							table.insert(open, nbNode)
+							pass = true
+						end
 						-- 2.c.iii - If it's already on OPEN, check if the new path is better. If it is, change the recorded PARENT, f, g and h values.
+						if not pass and table.contains( open, nbNode ) then
+							local oldF = nbNode.f()
+							local oldParent = nbNode.parent
+
+							nbNode.parent = currentNode
+							if oldF < nbNode.f() then
+								nbNode.parent = oldParent
+							end
+						end
+					end
 				end 
 			end
 		end
@@ -105,7 +152,7 @@ function astar.findPath(mapData, position, objective)
 		-- 2.d - Stop the process when:
 		if running then
 			-- 2.d.i - Found! Target Node is on CLOSE.
-			if currentNode.type == NodeType.Target then running = false end
+			if currentNode.type == astar.NodeType.Target then running = false end
 			-- 2.d.ii - Not Found! OPEN is empty.
 			if table.getn(open) == 0 then running = false end
 		end
@@ -115,7 +162,7 @@ function astar.findPath(mapData, position, objective)
 	-- 3 - Generate a path using the PARENT nodes.
 	result = {}
 
-	while currentNode and not currentNode.type == NodeType.Start do
+	while currentNode and not currentNode.type == astar.NodeType.Start do
 		table.insert(result, currentNode)
 		currentNode = currentNode.parent
 	end
@@ -123,3 +170,5 @@ function astar.findPath(mapData, position, objective)
 	-- 4 - Return the result.
 	return result
 end
+
+return astar
