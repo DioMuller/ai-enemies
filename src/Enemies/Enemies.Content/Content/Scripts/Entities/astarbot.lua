dfd09660a4d26fde2dbd3eb0aebeea4cc50f0305
Diogo Muller <diogo.muller@lactec.org.br>
2014-12-12 10:48:40 -0200

    WIP: More work on AStar Bot.

---
+++ src/Enemies/Enemies.Content/Content/Scripts/Entities/astarbot.lua
@@ -0,0 +1,35 @@
+script = {}
+
+function script.Initialize()
+	script.mapData = entity:GetMapLayout()
+	script.objective = entity:GetNearestTarget()
+
+	if script.objective and script.mapData then
+		script.path = astar.findPath(script.mapData, entity.Position, script.objective.Position, 
+		function(node, start, target)
+			local dx = target.position.X - node.position.X;
+            local dy = target.position.Y - node.position.Y;
+
+            return astar.directWeight * (dx * dx + dy * dy);
+		end)
+	end
+end
+
+function script.DoUpdate(delta)
+	entities = entity:GetNearbyEnemies()
+
+	if script.objective and script.mapData and not script.path then
+		script.path = astar.findPath(script.mapData, entity.Position, script.objective.Position, 
+		function(node, start, target)
+			local dx = target.position.X - node.position.X;
+            local dy = target.position.Y - node.position.Y;
+
+            return astar.directWeight * (dx * dx + dy * dy);
+		end)
+		for value in array.foreach(script.path) do
+			print("X = ", value.X, "Y = ", value.Y)
+		end
+	end
+end
+
+return script
--- src/Enemies/Enemies.Content/Content/Scripts/Libs/astar.lua
+++ src/Enemies/Enemies.Content/Content/Scripts/Libs/astar.lua
@@ -8,6 +8,30 @@ function table.contains(table, element)
   return false
 end
 
+function table.to_string (tt, indent, done)
+  done = done or {}
+  indent = indent or 0
+  if type(tt) == "table" then
+    for key, value in pairs (tt) do
+      io.write(string.rep (" ", indent)) -- indent it
+      if type (value) == "table" and not done [value] then
+        done [value] = true
+        io.write(string.format("[%s] => table\n", tostring (key)));
+        io.write(string.rep (" ", indent+4)) -- indent it
+        io.write("(\n");
+        table.to_string (value, indent + 7, done)
+        io.write(string.rep (" ", indent+4)) -- indent it
+        io.write(")\n");
+      else
+        io.write(string.format("[%s] => %s\n",
+            tostring (key), tostring(value)))
+      end
+    end
+  else
+    io.write(tt .. "\n")
+  end
+end
+
 astar = {}
 
 astar.directWeight = 10
@@ -21,25 +45,15 @@ astar.NodeType =
 	End = 'O'
 }
 
-function astar.createNode(position, f, g, h)
+function astar.createNode(position)
 	node = {}
-	node.position = position
-	node.f = function()
-			return g() + h
-		end
-	node.g = function()
-		if type == astar.NodeType.Start then return 0 end
-		if not parent then return 0 end
-
-		if position.X == parent.Position.X or position.Y == parent.Position.Y then
-			return parent.g() + astar.directWeight
-		else
-			return parent.g() + astar.diagonalWeight
-		end
-	end
+	
 	node.h = 0
 	node.parent = nil
 	node.type = astar.NodeType.Path
+	node.position = position
+	node.g = 0
+	node.f = 0
 
 	return node
 end
@@ -50,15 +64,15 @@ function astar.createGrid(mapData)
 	grid.width = mapData.TileCount.X
 	grid.height = mapData.TileCount.Y
 
-	print("G S = ", grid.width, grid.height )
+	local i,j = 0,0
 
 	for i=1, mapData.TileCount.X do
 		grid[i] = {}
 		for j=1, mapData.TileCount.Y do
 			-- Create node base
-			grid[i][j] = astar.createNode(position, f, g, h)
+			grid[i][j] = astar.createNode({X = i - 1, Y = j - 1})
 			
-			if mapData.CanGo(i, j) then
+			if mapData:CanGo(i - 1, j - 1) then
 				grid[i][j].type = astar.NodeType.Path
 			else
 				grid[i][j].type = astar.NodeType.Obstacle
@@ -91,71 +105,90 @@ function astar.findPath(mapData, position, objective, heuristic)
 
 	-- 1 - Add Start Node to the OPEN List.
 	table.insert(open, start)
-	print("Starting AStar")
 
 	-- 2 - Repeat:
 	while running do
 		-- 2.a - Search Node with the lowest F cost on the OPEN list.
 		local currentNode = nil
-		local indexNode = -1
-		local currentIndex = 0
 
-		for _, node in ipairs ( open ) do
+		for key, node in ipairs ( open ) do
 			if currentNode then
-				if currentNode.f() > node.f() then
+				if currentNode.f > node.f then
 					currentNode = node
-					indexNode = currentIndex
 				end
 			else
 				currentNode = node
-				indexNode = currentIndex
 			end
-
-			currentIndex = currentIndex + 1
 		end
 
-		print("Current Node is X = ", currentNode.position.X, "Y = ", currentNode.position.Y)
+		--print("Current Node is X = ", currentNode.position.X, "Y = ", currentNode.position.Y)
 
-		-- 2.b - Move this node to the CLOSE list.
-		table.insert(close, start)
-		table.remove(open, indexNode)
+		if currentNode then
+			-- 2.b - Move this node to the CLOSE list.
+			table.insert(close, currentNode)
 
-		-- 2.c - For each neighbour node:
-		for i = -1, 2 do
-			iPos = i + currentNode.position.X
+			i=1
+			while i <= #open do
+				if open[i] == nbNode then
+					table.remove(open, i)
+				else
+					i = i + 1
+				end
+			end
 
-			if iPos >= 0 and iPos < grid.width then
-				for j = -1, 2 do
-					jPos = j + currentNode.position.Y
-					if jPos >= 0 and jPos < grid.height then
-						-- 2.c.0 - Calculate Heuristic
-						nbNode = nodes[iPos][jPos]
-						nbNode.h = heuristic(nbNode, start, target)
+			-- 2.c - For each neighbour node:
+			for i = -1, 1 do
+				iPos = i + currentNode.position.X
+
+				if iPos >= 1 and iPos <= grid.width then
+					for j = -1, 1 do
+						jPos = j + currentNode.position.Y
+						if jPos >= 1 and jPos <= grid.height then
+							nbNode = nodes[iPos][jPos]
+
+							-- 2.c.0 - Calculate Values
+							if nbNode then
+								nbNode.h = heuristic(nbNode, start, target)
+								local g = 0
+								local f = 0
+
+								if nbNode.position.X == currentNode.position.X or nbNode.position.Y == currentNode.position.Y then
+									g = currentNode.g + astar.directWeight
+								else
+									g = currentNode.g + astar.diagonalWeight
+								end
+
+								f = g + nbNode.h
 						
-						local pass = false
+								local pass = false
 						 
-						-- 2.c.i - Ignore if it's an obstacle or if it's already on the CLOSE list.
-						if nbNode.type == astar.NodeType.Obstacle or table.contains(close, nbNode) then
-							pass = true
-						end
-						-- 2.c.ii - If it's not on OPEN, add to open and set the Node PARENT to CURRENT. Record f, g and h.
-						if not pass and not table.contains(open, nbNode ) then
-							nbNode.parent = currentNode
-							table.insert(open, nbNode)
-							pass = true
-						end
-						-- 2.c.iii - If it's already on OPEN, check if the new path is better. If it is, change the recorded PARENT, f, g and h values.
-						if not pass and table.contains( open, nbNode ) then
-							local oldF = nbNode.f()
-							local oldParent = nbNode.parent
-
-							nbNode.parent = currentNode
-							if oldF < nbNode.f() then
-								nbNode.parent = oldParent
+								-- 2.c.i - Ignore if it's an obstacle or if it's already on the CLOSE list.
+								if nbNode.type == astar.NodeType.Obstacle or table.contains(close, nbNode) then
+									pass = true
+									--print("Node ", nbNode.position.X, ",", nbNode.position.Y, " already on CLOSE")
+								end
+								-- 2.c.ii - If it's not on OPEN, add to open and set the Node PARENT to CURRENT. Record f, g and h.
+								if not pass and not table.contains(open, nbNode ) then
+									nbNode.parent = currentNode
+									nbNode.g = g
+									nbNode.f = f
+									table.insert(open, nbNode)
+									pass = true
+								end
+								-- 2.c.iii - If it's already on OPEN, check if the new path is better. If it is, change the recorded PARENT, f, g and h values.
+								if not pass and table.contains( open, nbNode ) then
+									if f < nbNode.f then
+										nbNode.parent = currentNode
+										nbNode.g = g
+										nbNode.f = f
+									end
+								end
 							end
 						end
-					end
-				end 
+					end 
+				else
+					running = false
+				end
 			end
 		end
 
@@ -164,7 +197,7 @@ function astar.findPath(mapData, position, objective, heuristic)
 			-- 2.d.i - Found! Target Node is on CLOSE.
 			if currentNode.type == astar.NodeType.Target then running = false end
 			-- 2.d.ii - Not Found! OPEN is empty.
-			if table.getn(open) == 0 then running = false end
+			if #open == 0 then running = false end
 		end
 
 	end
